6ab592b4aa6c60d526f554f25f91a2a1c42022ab
yhsuh <yhsuh@etri.re.kr>
2017-09-28 17:13:20 +0900

    2017/09/28

---
+++ org.etri.slice.core/src/main/java/org/etri/slice/core/perception/EventSubscriber.java
@@ -0,0 +1,57 @@
+/**
+ *
+ * Copyright (c) 2017-2017 SLICE project team (yhsuh@etri.re.kr)
+ * http://slice.etri.re.kr
+ *
+ * This file is part of The SLICE components
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with The SLICE components; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package org.etri.slice.core.perception;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Validate;
+import org.apache.felix.ipojo.handlers.event.Publishes;
+import org.apache.felix.ipojo.handlers.event.Subscriber;
+import org.apache.felix.ipojo.handlers.event.publisher.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+@Instantiate
+public class EventSubscriber {
+
+	private static Logger s_logger = LoggerFactory.getLogger(EventSubscriber.class);	
+	
+	@Publishes(name="pub", topics="foo,bar")
+	private Publisher m_publisher;	
+	
+	@Validate
+	public void start() {
+		s_logger.info("EventSubscriber started");
+	}
+	
+	@Invalidate
+	public void stop() {
+		s_logger.info("EventSubscriber stopped");
+	}
+	
+	@Subscriber(name="sub", topics="xxx", dataKey="aircon.data", dataType="java.lang.String")
+	public void receive(String xxx) {
+		System.out.println("Data received = " + xxx);
+	}
+}
