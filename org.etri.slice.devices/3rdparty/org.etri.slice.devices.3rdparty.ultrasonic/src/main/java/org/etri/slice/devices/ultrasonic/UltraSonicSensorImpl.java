4b26ba0ed9da2c6654436911226edeed35942f07
yhsuh <yhsuh@yhsuh-imac.local>
2018-01-23 12:18:16 +0900

    2018/01/23

--- org.etri.slice.devices/3rdparty/org.etri.slice.devices.3rdparty.ultrasonic/src/main/java/org/etri/slice/devices/ultrasonic/UltraSonicSensorImpl.java
+++ org.etri.slice.devices/3rdparty/org.etri.slice.devices.3rdparty.ultrasonic/src/main/java/org/etri/slice/devices/ultrasonic/UltraSonicSensorImpl.java
@@ -19,8 +19,7 @@
  * along with The SLICE components; see the file COPYING.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
-package org.etri.slice.devices.pressuresensor;
-
+package org.etri.slice.devices.ultrasonic;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
@@ -35,54 +34,50 @@ import org.apache.felix.ipojo.annotations.Validate;
 import org.apache.felix.ipojo.handlers.event.Publishes;
 import org.apache.felix.ipojo.handlers.event.publisher.Publisher;
 import org.apache.http.annotation.GuardedBy;
-import org.etri.slice.commons.Sensor;
-import org.etri.slice.commons.car.event.Pressure;
+import org.etri.slice.commons.car.ObjectInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
 @Component
 @Instantiate
-public class PressureSensorImpl implements Sensor, Runnable {
+public class UltraSonicSensorImpl implements Runnable {
 	
-	private static Logger s_logger = LoggerFactory.getLogger(PressureSensorImpl.class);	
+	private static Logger s_logger = LoggerFactory.getLogger(UltraSonicSensorImpl.class);	
 	
-	@Property(name="interval", value="1000")
+	@Property(name="interval", value="500")
 	private long m_interval;
 	
 	private Lock m_lock = new ReentrantLock();
 	@GuardedBy("m_lock") private Condition m_stopCondition = m_lock.newCondition();
-	@GuardedBy("m_lock") private volatile boolean m_stopRequested = false;
-
-	@Publishes(name="PressureSensorImpl", topics=Pressure.topic, dataKey=Pressure.dataKey)
-	private Publisher m_publisher;	
+	@GuardedBy("m_lock") private volatile boolean m_stopRequested = false;	
 	
-	private GpioController m_gpio;	
-	private GpioPinDigitalInput m_PGP;	
-	private PRESS_MODE press_mode = PRESS_MODE.NONE;
+	@Publishes(name="UltraSonicSensor", topics=ObjectInfo.topic, dataKey=ObjectInfo.dataKey)
+	private Publisher m_publisher;
 	
-	private enum PRESS_MODE{
-		UP,
-		DOWN,
-		NONE
-	}
+	GpioController gpio;
 	
-	public PressureSensorImpl() {
-
-	}
+	//A0,B0,C0,PWM0
+	GpioPinDigitalOutput TRIGER;
+	GpioPinDigitalInput ECHO;	
 	
 	@Validate
 	public void start() {
-		m_gpio = GpioFactory.getInstance();		
-		m_PGP = m_gpio.provisionDigitalInputPin(RaspiPin.GPIO_13, PinPullResistance.PULL_UP); //Press, Gpio, Pin, GPIO_09
+		System.out.println("Ultra Gpio Init");
+		gpio = GpioFactory.getInstance();
+		
+		TRIGER = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_24, PinState.LOW); //down, direct, A, Gpio, Pin, GPIO_19
+		ECHO = gpio.provisionDigitalInputPin(RaspiPin.GPIO_23, PinPullResistance.PULL_DOWN); //up, down, CS, Gpio, Pin, GPIO_13		
 		
 		new Thread(this).start();
-		s_logger.info("PressSensor started.");
+		s_logger.info("UltraSonicSensor started.");
 	}
 	
 	@Invalidate
@@ -91,38 +86,45 @@ public class PressureSensorImpl implements Sensor, Runnable {
 		m_stopRequested = true;
 		m_stopCondition.signal();
 		m_lock.unlock();
-		m_gpio.shutdown();
-		s_logger.info("PressSensor stoppted");		
-	}	
-	
+		gpio.shutdown();
+		s_logger.info("UltraSonicSensor stoppted");		
+	}
+
 	@Override
 	public void run() {
+		long pulseStart = 0, pulseEnd = 0;
 		m_lock.lock();
 		try {
 			while ( !m_stopRequested ) {
-				if ( m_PGP.isLow() ) {
-					if( press_mode == PRESS_MODE.NONE || press_mode == PRESS_MODE.DOWN ) {
-						press_mode = PRESS_MODE.UP;
-						Pressure detected = Pressure.builder().value(0).build();
-						s_logger.info("PUB: " + detected);
-					}
-				}
-				else {
-					if( press_mode == PRESS_MODE.NONE || press_mode == PRESS_MODE.UP ) {
-						press_mode = PRESS_MODE.DOWN;
-						Pressure detected = Pressure.builder().value(50).build();
-						s_logger.info("PUB: " + detected);
-					}
-				}
+				TRIGER.low();
 				m_stopCondition.await(m_interval, TimeUnit.MILLISECONDS);
+				
+				TRIGER.high();
+				m_stopCondition.await(1, TimeUnit.MICROSECONDS);
+				TRIGER.low();
+				
+				while ( ECHO.getState() == PinState.LOW ) {
+					pulseStart = System.currentTimeMillis();
+				}
+				
+				while ( ECHO.getState() == PinState.HIGH ) {
+					pulseEnd = System.currentTimeMillis();
+				}
+				
+				long pulseDuration = pulseEnd - pulseStart;
+				long dist = pulseDuration * 17000 / 100;
+				
+				ObjectInfo distance = ObjectInfo.builder().objectId("obj").distance(dist).build();
+				m_publisher.sendData(distance);			
+				s_logger.info("PUB: " + distance);
 			}
-        }
+		}
 		catch ( Exception e ) {
 	        s_logger.error("gpio shutdown!!");
-        }
+		}
 		finally {
 			m_lock.unlock();
-			m_gpio.shutdown();
+			gpio.shutdown();
 		}
 	}
 }
